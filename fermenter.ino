1bbb01e3af41a064cae1f11342e270b7e517403a
Chris Johnston <christopermichaeljohnston@gmail.com>
2018-03-05 13:08:39 -0500

    refactor

--- fermenter.ino
+++ fermenter.ino
@@ -1,10 +1,10 @@
-#define VERSION 0.81
-#define TYPE 'F'
+#define VERSION 2.0
+#define TYPE "FERMENTER"
 
 #include <string.h>
 #include <EEPROM.h>
-#include <OneWire.h>
 #include <DallasTemperature.h>
+#include <OneWire.h>
 
 //
 // configuration with defaults
@@ -23,6 +23,7 @@ struct device {
 #define SENSOR_DELAY 1000
 OneWire oneWire(ONE_WIRE);
 DallasTemperature sensors(&oneWire);
+
 struct sensorStateMachine {
   unsigned long sensors   = 0;  
 } mySensorStateMachine;
@@ -38,22 +39,20 @@ const int PUMP[] = {4,5};
 #define OFF  0
 #define CHILL 1
 #define HEAT  2
+
 struct fermenterConfig {
-  char name[17]     = "";
   int mode         = OFF;
   float setpoint   = 64.0;
   float hysteresis = 0.1;
-  float rampStep   = 0.0;
-  float rampPoint  = 68.0;
-  unsigned long rampDelay = 3600000;
   unsigned long pumpRun   = 5000;
   unsigned long pumpDelay = 300000;
 };
+
 struct fermenterStateMachine {
-  unsigned long rampDelay = 0;
-  unsigned long pump      = 0;
+  unsigned long pumpRun   = 0;
   unsigned long pumpDelay = 0;
 };
+
 struct fermenter {
   struct fermenterConfig config;
   struct fermenterStateMachine stateMachine;
@@ -65,6 +64,7 @@ struct fermenter {
 // buffer for serial communication
 //
 #define BUFFER_SIZE 64
+
 struct buffer {
   char buffer[BUFFER_SIZE+1];
   int index = 0;
@@ -80,7 +80,6 @@ void setup() {
   setupSensors();
   setupPumps();
   setupConfig();
-  Serial.println("Ready...");
 }
 
 void loop() {
@@ -103,8 +102,6 @@ void setupConfig() {
     for (int i=0 ; i<16 ; i++) {
       myDevice.sn[i] = '0'+random(10);
     }
-    strcpy(myFermenter[0].config.name,"0");
-    strcpy(myFermenter[1].config.name,"1");
     sensors.getAddress(myFermenter[0].deviceAddress, 0);
     sensors.getAddress(myFermenter[1].deviceAddress, 1);
     saveConfig();
@@ -175,20 +172,12 @@ void runCommand(char * cmd, char * fermenter, char * param) {
     Serial.println(TYPE);
   } else if (strcmp(cmd,"getSN") == 0) {
     Serial.println(myDevice.sn);
-  } else if (strcmp(cmd,"getName") == 0) {
-    Serial.println(myFermenter[atoi(fermenter)].config.name);
   } else if (strcmp(cmd, "getMode") == 0) {
     Serial.println(myFermenter[atoi(fermenter)].config.mode);
   } else if (strcmp(cmd,"getSetpoint") == 0) {
     Serial.println(myFermenter[atoi(fermenter)].config.setpoint);
   } else if (strcmp(cmd,"getHysteresis") == 0) {
     Serial.println(myFermenter[atoi(fermenter)].config.hysteresis);
-  } else if (strcmp(cmd, "getRampStep") == 0) {
-    Serial.println(myFermenter[atoi(fermenter)].config.rampStep);
-  } else if (strcmp(cmd, "getRampPoint") == 0) {
-    Serial.println(myFermenter[atoi(fermenter)].config.rampPoint);
-  } else if (strcmp(cmd, "getRampDelay") == 0) {
-    Serial.println(myFermenter[atoi(fermenter)].config.rampDelay);
   } else if (strcmp(cmd,"getPumpRun") == 0) {
     Serial.println(myFermenter[atoi(fermenter)].config.pumpRun);
   } else if (strcmp(cmd,"getPumpDelay") == 0) {
@@ -198,20 +187,12 @@ void runCommand(char * cmd, char * fermenter, char * param) {
   } else if (strcmp(cmd,"getDeviceAddress") == 0) {
     printAddress(myFermenter[atoi(fermenter)].deviceAddress);
     Serial.println();
-  } else if (strcmp(cmd,"setName") == 0) {
-    setName(atoi(fermenter),param);
   } else if (strcmp(cmd,"setMode") == 0) {
     setMode(atoi(fermenter),atoi(param));
   } else if (strcmp(cmd,"setSetpoint") == 0) {
     setSetpoint(atoi(fermenter),atof(param));
   } else if (strcmp(cmd,"setHysteresis") == 0) {
     setHysteresis(atoi(fermenter),atof(param));
-  } else if (strcmp(cmd,"setRampStep") == 0) {
-    setRampStep(atoi(fermenter),atof(param));
-  } else if (strcmp(cmd,"setRampPoint") == 0) {
-    setRampPoint(atoi(fermenter),atof(param));
-  } else if (strcmp(cmd,"setRampDelay") == 0) {
-    setRampDelay(atoi(fermenter),strtoul(param, NULL, 10));
   } else if (strcmp(cmd, "setPumpRun") == 0) {
     setPumpRun(atoi(fermenter),strtoul(param, NULL, 10));
   } else if (strcmp(cmd, "setPumpDelay") == 0) {
@@ -225,16 +206,6 @@ void printAddress(DeviceAddress deviceAddress) {
   for (uint8_t i = 0; i < 8; i++) {
     if (deviceAddress[i] < 16) Serial.print("0");
     Serial.print(deviceAddress[i], HEX);
-  }
-}
-
-void setName (int fermenter, char * name) {
-  if (strlen(name) < 16) {
-    strcpy(myFermenter[fermenter].config.name, name);
-    saveConfig();
-    Serial.println("set");
-  } else {
-    Serial.println("string too long (16 max)");
   }
 }
 
@@ -274,35 +245,6 @@ void setHysteresis(int fermenter, float hysteresis) {
   }
 }
 
-void setRampStep(int fermenter, float rampStep) {
-  if (rampStep >= -1.0 && rampStep <= 1.0) {
-    myFermenter[fermenter].config.rampStep = rampStep;
-    saveConfig();
-    Serial.println("set");
-  } else {
-    Serial.println("out or range (-1.0 to 1.0)");
-  }
-}
-
-void setRampPoint(int fermenter, float rampPoint) {
-  if (rampPoint >= 32.0 && rampPoint <= 212.0) {
-    myFermenter[fermenter].config.rampPoint = rampPoint;
-    saveConfig();
-    Serial.println("set");
-  } else {
-    Serial.println("out or range (0.1 to 1.0)");
-  }
-}
-
-void setRampDelay(int fermenter, unsigned long rampDelay) {
-  if (rampDelay >= 3600000 && rampDelay <= 86400000) {
-    myFermenter[fermenter].config.rampDelay = rampDelay;
-    saveConfig();
-    Serial.println("set");
-  } else {
-    Serial.println("out or range (3600000 to 86400000)");
-  }
-}
 
 void setPumpRun(int fermenter, unsigned long pumpRun) {
   if (pumpRun >= 1000 && pumpRun <= 60000) {
@@ -371,42 +313,29 @@ void getTemperatures() {
 
 void loopFermenter(int fermenter) {
   if (myFermenter[fermenter].stateMachine.pumpDelay == 0) {
-    if (myFermenter[fermenter].stateMachine.pump == 0) {
+    if (myFermenter[fermenter].stateMachine.pumpRun == 0) {
       if ((myFermenter[fermenter].config.mode == CHILL && ((myFermenter[fermenter].temperature-myFermenter[fermenter].config.setpoint) >= myFermenter[fermenter].config.hysteresis)) ||
           (myFermenter[fermenter].config.mode == HEAT  && ((myFermenter[fermenter].temperature-myFermenter[fermenter].config.setpoint) <= (-myFermenter[fermenter].config.hysteresis)))) {
         onPump(fermenter);
       }
     } else {
-      if ((millis() - myFermenter[fermenter].stateMachine.pump) >= myFermenter[fermenter].config.pumpRun) {
+      if ((millis() - myFermenter[fermenter].stateMachine.pumpRun) >= myFermenter[fermenter].config.pumpRun) {
         offPump(fermenter);
       }
     }
   } else if ((millis() - myFermenter[fermenter].stateMachine.pumpDelay) >= myFermenter[fermenter].config.pumpDelay) {
     myFermenter[fermenter].stateMachine.pumpDelay = 0;
   }
-  if (myFermenter[fermenter].config.rampStep != 0.0) {
-    if (myFermenter[fermenter].stateMachine.rampDelay == 0) {
-      myFermenter[fermenter].config.setpoint += myFermenter[fermenter].config.rampStep;
-      if (abs(myFermenter[fermenter].config.rampPoint-myFermenter[fermenter].config.setpoint) < abs(myFermenter[fermenter].config.rampStep)){
-        myFermenter[fermenter].config.setpoint = myFermenter[fermenter].config.rampPoint;
-        myFermenter[fermenter].config.rampStep = 0;
-      }
-      saveConfig();
-      myFermenter[fermenter].stateMachine.rampDelay = millis();
-    } else if ((millis() - myFermenter[fermenter].stateMachine.rampDelay) >= myFermenter[fermenter].config.rampDelay) {
-      myFermenter[fermenter].stateMachine.rampDelay = 0;
-    }
-  }
 }
 
 void offPump(int fermenter) {
-  myFermenter[fermenter].stateMachine.pump = 0;
+  myFermenter[fermenter].stateMachine.pumpRun = 0;
   myFermenter[fermenter].stateMachine.pumpDelay = millis();
   digitalWrite(PUMP[fermenter], HIGH);
 }
 
 void onPump(int fermenter) {
-  myFermenter[fermenter].stateMachine.pump = millis();
+  myFermenter[fermenter].stateMachine.pumpRun = millis();
   myFermenter[fermenter].stateMachine.pumpDelay = 0; 
   digitalWrite(PUMP[fermenter], LOW);
 }
